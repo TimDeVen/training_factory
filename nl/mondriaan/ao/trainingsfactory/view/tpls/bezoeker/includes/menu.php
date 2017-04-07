35a18d9d9b1c26d05c11d1a20fe4922bd62e2e3b
Pim <pim_1994@hotmail.com>
2017-04-07 11:41:21 +0200

    includes aangepast

--- nl/mondriaan/ao/trainingsfactory/view/tpls/bezoeker/includes/menu.php
+++ nl/mondriaan/ao/trainingsfactory/view/tpls/bezoeker/includes/menu.php
@@ -1,2 +1,29 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    
+    <ul class="nav navbar-nav">
+      <li><a href="?control=bezoeker&action=default">Home</a></li>
+      <li><a href="#">Trainings Aanbod</a></li>
+      <li class="active"><a href="?control=bezoeker&action=lidWorden">Lid worden</a></li>
+      <li><a href="#">Gedrags regels</a></li>
+      <li><a href="#">Locatie & Contact</a></li>
+    </ul>
+  </div>
+</nav>
 
+<div class="row">
+    <div class="col-xs-6 col-md-4">
+        <center><img src="img/vechtsport1.jpg" alt="" width="250px" height="200px"/></center>
+        
+    </div>
+
+    <div class="col-xs-6 col-md-4">
+        <img src="img/vechtsport2.jpg" alt="" width="500px" height="200px"/>
+    </div>
+
+    <div class="col-xs-6 col-md-4">
+        <center><img src="img/vechtsport3.jpg" alt="" width="250px" height="200px"/></center>
+    </div>
+    
+</div>
 
