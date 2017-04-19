73f68539b1bd8544150b147595fac34d75bc78c8
Casey Vianen <c.vianen@mondriaanict.nl>
2017-04-19 15:02:45 +0200

    Overzicht inschrijvingen

--- nl/mondriaan/ao/trainingsfactory/models/MemberModel.php
+++ nl/mondriaan/ao/trainingsfactory/models/MemberModel.php
@@ -29,4 +29,28 @@ class MemberModel extends AbstractModel
     $_SESSION = array();
     session_destroy();
     }
+    public function getIngeschrevenLessen()
+    {
+        $sql=' SELECT DATE_FORMAT(`lessons`.`date`, "%d-%m-%Y") as `datum`,
+           DATE_FORMAT(`lessons`.`time`,"%H:%i") as `tijd`,
+           `trainingen`.`extra_costs` as `prijs`,
+           `lessons`.`id` as `id`,
+           `trainingen`.`description` as `soort` ,
+           `lessons`.`max_persons` as `max_deelnemers`,
+           `registrations` . `payment` as `betaald`
+           FROM `lessons`
+            JOIN `trainingen` on `lessons`.`training_id` = `trainingen`.`id`
+            JOIN `registrations` on `lessons`.`id` = `registrations`.`id`
+            WHERE `lessons`.`id` IN (SELECT lesson_id FROM `registrations`
+                                    WHERE `registrations`.`person_id`=:id)
+            order by  DATE(`lessons`.`date`)';
+
+        $stmnt = $this->dbh->prepare($sql);
+        $id=$this->getGebruiker()->getId();
+        $stmnt->bindParam(':id',$id );
+        $stmnt->execute();
+        $activiteiten = $stmnt->fetchAll(\PDO::FETCH_CLASS,__NAMESPACE__.'\db\Les');
+        return $activiteiten;
+    }
+  
 }
