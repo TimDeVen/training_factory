097be2649b78c341238fa5d65e1907047862271a
Casey Vianen <c.vianen@mondriaanict.nl>
2017-04-19 19:31:27 +0200

    Zuka Ivan noob

--- nl/mondriaan/ao/trainingsfactory/view/tpls/instructeur/includes/footer.php
+++ nl/mondriaan/ao/trainingsfactory/view/tpls/instructeur/includes/footer.php
@@ -1,34 +1,12 @@
 
+
     <footer class="footer">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                <ul>
-                    <li>
-                        Yo
-                    </li>
-
-                </ul>
-                </div>
-
-                <div class="col-md-4">
-                    <ul>
-                        <li>
-                            Yo
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div class="col-md-4">
-                    <ul>
-                        <li>
-                            Yo
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
+              <div class="col-md-4 col-md-offset-4">
+                  Training Centrum Den Haag <strong>010 519 0532</strong> <br>
+                  development bij Fast Development 2016
+              </div>
 
         </div>
     </footer>
